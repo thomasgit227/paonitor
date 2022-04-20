22c96e9388bd91d350c6eefbfe2f9bf3dc4029c8
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-19 22:47:00 -0500

    no message

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-
-const app = express()
-
-app.use(bodyParser.json({limit: "32mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "32mb", extended: true}))
-app.use(cors())
