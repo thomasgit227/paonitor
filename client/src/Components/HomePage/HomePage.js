e041d1e6b2e714163614e03c7b25c9c2d8b3b706
Austin Benton <austin@aebenton.com>
2022-04-20 01:45:43 -0500

    Styling Changes (Responsive)

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -2,31 +2,26 @@ import React from 'react';
 import { Button } from 'reactstrap';
 import './HomePage.css';
 
-const Home = () =>{
+const Home = () => {
 
     return (
         <div class="pageContainer">
-            
+
             <h1 class="head">Paonitor Control Panel</h1>
 
             <div class="buttonContainer">
-               
-                <div className="col1">
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 1</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 2</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 3</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 4</Button>{' '}
-                </div>
-                
-                <div className="col2">
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 5</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 6</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 7</Button>{' '}
-                    <Button style={{backgroundColor: '#384D48'}} size="lg" className="buttonStyle">Button 8</Button>{' '}
-                </div>
-                
+
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 1</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 2</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 3</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 4</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 5</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 6</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 7</Button>{' '}
+                <Button style={{ backgroundColor: '#384D48' }} size="lg" className="buttonStyle">Button 8</Button>{' '}
+
             </div>
-            
+
         </div>
     );
 
