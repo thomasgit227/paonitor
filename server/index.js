c8f0ce5c8ece4be93852a859f8a56927fa57594b
Austin Benton <austin@aebenton.com>
2022-04-20 00:04:24 -0500

    API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+
+var program;
 
 const app = express()
 
-app.use(bodyParser.json({limit: "32mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "32mb", extended: true}))
-app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+// app.use(cors())
+
+app.get('/pi/', (req, res) => {
+    res.status(200);
+    res.send(program);
+})
+
+app.post('/new-prog/', (req, res) => {
+    let prog = req.body
+    
+    program = prog;
+    res.status(201);
+    res.send(program);
+})
