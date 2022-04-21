6c9cc21ad0697de331f2e518d3341588f2b5a1ec
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-21 18:12:48 -0500

    npm installs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import path = from 'path';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 const PORT = process.env.PORT || 5000
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 var program;
 
 const app = express()
