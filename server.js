17f2d3ad5f7fc68174373642a2aabfdbae5463f9
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-22 13:52:21 -0500

    endpoint testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-var currentAction = '';
+var currentAction = 'nothing';
 
 // Middleware
 app.use(cors());
@@ -19,22 +19,23 @@ app.use(bodyParser.json());
 // Import Built Files
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// Use Built Files
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
 // Endpoints
 app.get('/action/', (req, res) => {
+  console.log(currentAction)
   res.send(currentAction);
   res.status(200);
 })
 
 app.post('/action/', (req, res) => {
-  currentAction = req.body.new;
+  currentAction = req;
   res.send(currentAction);
   res.status(201);
 })
+
+// Use Built Files
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // Listener
 app.listen(PORT, () => {
