cf9fa237a33c74bd42db4d282313b0c9f6140347
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-21 19:26:35 -0500

    new express.static dir

--- server/index.js
+++ server/index.js
@@ -11,29 +11,28 @@ var program;
 
 const app = express()
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'public')))
-    app.use(express.json())
-    app.set('views', path.join(__dirname, 'views'))
-    app.set('view engine', 'ejs')
-    app.use(cors())
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../', 'client')))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-    app.get('/pi/', (req, res) => {
-        res.status(200);
-        res.send(program);
-    })
+app.get('/pi/', (req, res) => {
+    res.status(200);
+    res.send(program);
+})
 
-    app.post('/new-prog/', (req, res) => {
-        let prog = req.body.new;
-        
-        program = prog;
-        res.status(201);
-        res.send(program);
-    })
+app.post('/new-prog/', (req, res) => {
+    let prog = req.body.new;
+    
+    program = prog;
+    res.status(201);
+    res.send(program);
+})
 
-    app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname,"../", "client", "src", "index.js"));
-    });
-}
+//     res.sendFile(path.resolve(__dirname,"../", "client", "src", "index.js"));
+// });
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
