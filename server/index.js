da56fdfabc62031456c1bc1d5cfc0505bcebedfc
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-21 20:59:12 -0500

    goto static built files again again

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.post('/new-prog/', (req, res) => {
 
 app.get("*", (req, res) => {
 
-    res.sendFile(path.resolve(__dirname,"../", "client", "build"));
+    res.sendFile(path.resolve(__dirname,"../", "client", "build","index.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
