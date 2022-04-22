56af0ab6a692afc9b307636b89dda7150b81fadf
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-21 19:14:05 -0500

    Directory Edits

--- server/index.js
+++ server/index.js
@@ -33,8 +33,7 @@ if(process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname,"../", "client", "build", "index.js"));
-    
+        res.sendFile(path.resolve(__dirname,"../", "client", "src", "Components", "index.js"));
     });
 }
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
