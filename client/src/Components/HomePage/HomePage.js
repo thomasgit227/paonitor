451ce3da3612bd4a8d56cf2eb62ce366ce7d64fa
Thomas Floyd <tefloyd1215@gmail.com>
2022-04-23 02:10:16 -0500

    Actual Commands

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -10,14 +10,10 @@ const Home = () => {
             <h1 class="head">Paonitor Control Panel</h1>
 
             <div class="buttonContainer">
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
-                <ProgramCard title="Snake" description="Classic snake arcade game"/>
+                <ProgramCard title="Snake" description="Arcade Game"/>
+                <ProgramCard title="Stacker" description="Arcade Game"/>
+                <ProgramCard title="Pulse" description="Visuals"/>
+                <ProgramCard title="Rave" description="Visuals"/>
             </div>
         </div>
     );
